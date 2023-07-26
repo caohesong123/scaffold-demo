f9c1f3f413b69ee877a438deeaeb81f44b109bc1
caohesong123 <songhe430gmail.com>
2023-07-26 14:35:59 +0800

    token解析

--- mian.go
+++ mian.go
@@ -20,6 +20,15 @@ func main() {
 	//测试生成jwt token是否可用
 	ss, _ := jwtutils.GenToken("song")
 	fmt.Println("是否能生成TOKEN", ss)
+	//验证解析Token的方法
+	claims, err := jwtutils.ParseToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNvbmciLCJpc3MiOiJkb3RiYWxvIiwic3ViIjoic29uZyIsImV4cCI6MTY5MDM2MDQ2NCwibmJmIjoxNjkwMzUzMjY0LCJpYXQiOjE2OTAzNTMyNjR9.DJgMu-zkYc-KQr_pjNKKlkJKgCJtUH4Iw3IomUfbgGY")
+	if err != nil {	
+		//解析失败
+		fmt.Println("解析失败：",err.Error())
+	}else {
+		fmt.Println("解析成功：。。。。。。",claims)
+	}
+
 	r.Run(config.Port)
 
 }
--- utils/jwtutils/jwtutils.go
+++ utils/jwtutils/jwtutils.go
@@ -1,7 +1,9 @@
 package jwtutils
 
 import (
+	"errors"
 	"scaffold-demo/config"
+	"scaffold-demo/utils/logs"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -18,7 +20,7 @@ type MyCustomClaims struct {
 // 2.封装生成tonken的函数
 func GenToken(username string) (string, error) {
 	claims := MyCustomClaims{
-		"bar",
+		username,
 		jwt.RegisteredClaims{
 			// A usual scenario is to set the expiration time relative to the current time
 			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Minute * time.Duration(config.JwtExpTime))),
@@ -31,4 +33,23 @@ func GenToken(username string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	ss, err := token.SignedString(JwtSigKey)
 	return ss, err
+}
+
+// 3.解析token
+func ParseToken(ss string) (*MyCustomClaims, error) {
+	token, err := jwt.ParseWithClaims(ss, &MyCustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return JwtSigKey, nil
+	})
+	if err != nil {
+		//解析token失败
+		logs.Error(nil, "解析Token失败")
+		return nil, err
+	}
+	if claims, ok := token.Claims.(*MyCustomClaims); ok && token.Valid {
+		//说明TOKEN合法
+		return claims, nil
+	} else {
+		logs.Warning(nil, "token不合法")
+		return nil, errors.New("Token不合法:invalid token")
+	}
 }
